23334594c741ad7c4a77edd436af07a566bb75a4
Baz <jennigoldstein@gmail.com>
2018-08-24 16:26:08 -0500

    update

--- index.js
+++ index.js
@@ -1,70 +1,79 @@
-var Word= require("./word.js");
-// var Letter= require("./letter.js");
+var Word = require("./word.js");
 var inquirer = require("inquirer");
 
-var guessesLeft=8;
+var guessesLeft = 8;
 var words = ["beach", "sand", "ocean", "waves", "fish", "sun", "swimsuit", "towel", "seashell"];
 
 //choose random word from words array
 var randomWord = words[Math.floor(Math.random() * words.length)];
 
-var newWordTest = new Word (randomWord);
-//newWordTest.addLetters();
+var newWordTest = new Word(randomWord);
+
+console.log("Welcome to the Word Guess Game!");
+console.log("The theme is Beach Vacation...");
+
+var answerBlanks = [];
+for (var i = 0; i < randomWord.length; i++) {
+    answerBlanks[i] = (" _ ");
+}
+console.log(answerBlanks.join(""));
 
 //define functions
 
-
-
-
 function askForLetter(randomWord) {
-        
-        inquirer
-            .prompt([{
 
-                type: "input",
-                message: "Please guess a letter. \n You have " + guessesLeft + " guesses remaining",
-                name: "letter",
-                
-                    
-            
-            }]).then(function (answer) {
-                if (!randomWord.includes(answer.letter)){
-                    guessesLeft--;
-                }
-                
-                userGuess= answer.letter.toLowerCase();
+    inquirer
+        .prompt([{
 
-                newWordTest.check(userGuess);
-                console.log(newWordTest.displayWord());
+            type: "input",
+            message: "Please guess a letter. \n You have " + guessesLeft + " guesses remaining",
+            name: "letter",
 
-                if (newWordTest.displayWord("") === randomWord){
-                    console.log("You win!")
-                    reset();
-                }
-                
-                if (guessesLeft > 0){
-                    askForLetter(randomWord);
-                } else {
-                    console.log ("You Lose!");
-                    reset();
-                }
 
-                
-                
-            });
+
+        }]).then(function (answer) {
+            if (!randomWord.includes(answer.letter)) {
+                guessesLeft--;
+            }
+
+            userGuess = answer.letter.toLowerCase();
+
+            newWordTest.check(userGuess);
+            console.log(newWordTest.displayWord());
+
+            if (newWordTest.displayWord("") === randomWord) {
+                console.log("You win!")
+                reset();
+            }
+
+            if (guessesLeft > 0) {
+                askForLetter(randomWord);
+            } else {
+                console.log("You Lose!");
+                reset();
+            }
+
+
+
+        });
+}
+
+function reset() {
+    randomWord = words[Math.floor(Math.random() * words.length)];
+    newWordTest = new Word(randomWord);
+    guessesLeft = 8;
+    var answerBlanks = [];
+    for (var i = 0; i < randomWord.length; i++) {
+        answerBlanks[i] = (" _ ");
     }
+    console.log(answerBlanks.join(""));
+    askForLetter(randomWord);
+}
 
-    function reset(){
-        randomWord = words[Math.floor(Math.random() * words.length)];
-        newWordTest= new Word (randomWord);
-        guessesLeft=8;
-        askForLetter(randomWord);
-    }
 
-    
-    //start game
+//start game
 askForLetter(randomWord);
 
 
- 
- 
+
+
